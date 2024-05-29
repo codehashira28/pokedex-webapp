54b8d85467a8b400983d3334352f4d5cc373f382
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2024-05-28 22:56:08 -0500

    added logic to generate the generation list page

--- assets/generations.js
+++ assets/generations.js
@@ -7,6 +7,8 @@ class Pokemon {
     }
 }
 
+// function to get a pokemon's image, types, and stats and returns them in a pokemon object
+
 const getPokemonInfo = async (pokemonName) => {
     try {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`)
@@ -25,25 +27,62 @@ const getPokemonInfo = async (pokemonName) => {
         const stats = data.stats.map(statObj => {
             return `${statObj.stat.name}: ${statObj.base_stat}`;
         })
-
+        
         const pokemon = new Pokemon(pokemonName, pokeImageUrl, types, stats);
         return pokemon;
     } catch (err) {
-        console.error(err.message)
+        console.error("Error: ", err.message)
     }
 }
 
-const getGeneration = async (generation) => {
+// function to create the pokemon card that will be listed on the generation listing page
+
+const createPokemonTag = ({ name, image, types}) => {
+    let spans = "";
+    types.forEach(type => {
+        spans += `<span class="py-1 px-2 mx-1 border rounded text-center text-light fw-bold fs-6 ${type}">${type}</span>`;
+    })
+    return `<div class="card border-0 col"><a href="./pokemon.html" class="border rounded shadow-sm poke-card bg-light">
+                    <img src="${image}" alt="${name}" class="card-img-top">
+                    <div class="text-center mt-3">
+                        <h4 class="mb-4">${name.replace(name[0], name[0].toUpperCase())}</h4>
+                        <p class="lead d-flex flex-column align-items-center d-sm-block">
+                            ${spans}
+                        </p>
+                    </div>
+                </a>
+            </div>`
+}
+
+const loadingWheel = () => {
+    return `<div class="spinner-container d-flex justify-content-center">
+                <div class="spinner-border" role="status">
+                    <span class="sr-only"></span>
+                </div>
+            </div>`
+}
+
+// function that prints the entire generation list
+
+const printGeneration = async (generationNumber) => {
     try {
-        const response = await fetch(`https://pokeapi.co/api/v2/generation/${generation}/`);
+        const response = await fetch(`https://pokeapi.co/api/v2/generation/${generationNumber}/`);
         const data = await response.json();
         const pokemonArr = data.pokemon_species;
-        pokemonArr.forEach(async pokemon => {
-            const pokemonName = pokemon.name;
-            const pokeData = await getPokemonInfo(pokemonName);
-            console.log(pokeData)
-        })
+        const pokemonlist = document.getElementById('pokemon-list');
 
+        for(let i = 0; i < pokemonArr.length; ++i) {
+            const pokemonName = pokemonArr[i].name;
+            pokemonlist.insertAdjacentHTML("beforeend", loadingWheel());
+            const pokeData = await getPokemonInfo(pokemonName);
+            if(pokeData) {
+                const pokemonTag = createPokemonTag(pokeData);
+                pokemonlist.insertAdjacentHTML("beforeend", pokemonTag);
+                
+            }
+            document.querySelector('.spinner-container').remove();
+            
+        }
     } catch (err) {
         console.error(err)
     }
@@ -53,4 +92,4 @@ const getGeneration = async (generation) => {
 const heading = document.getElementById('generation-heading');
 heading.textContent = `Generation ${localStorage.getItem('generation')}`
 const generationNumber = localStorage.getItem('generation');
-getGeneration(generationNumber);
+printGeneration(generationNumber)
