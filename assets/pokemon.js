6415da43f2e1377708c79eaca2f14253f3a3d30c
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2024-06-19 19:09:49 -0500

    finished adding logic to generate pokemon evolution cycle

---
+++ assets/pokemon.js
@@ -0,0 +1,123 @@
+const getEvolutionCycle = async (pokemon) => {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}/`);
+        const data = await response.json();
+        const evolutionResponse = await fetch(data.evolution_chain.url);
+        const evolutionData = await evolutionResponse.json();
+        const evolutionCycle = [];
+        evolutionCycle.push(evolutionData.chain.species.name);
+        let evolutionChain = evolutionData.chain.evolves_to;
+        while(evolutionChain.length != 0) {
+            for(let i = 0; i < evolutionChain.length; ++i) {
+                console.log(evolutionChain[i].species.name)
+                evolutionCycle.push(evolutionChain[i].species.name);
+            }
+            evolutionChain = evolutionChain[0].evolves_to;
+        }
+
+        console.log(evolutionCycle);
+
+        return evolutionCycle;
+    } catch(err) {
+        console.error(err);
+    }
+    
+}
+
+const getEvolutionImages = async (cycle) => {
+    const evolutionImages = [];
+
+    try {
+        for(const pokemon of cycle) {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`);
+            const data = await response.json();
+            const imageType = 'official-artwork';
+            const pokeImageUrl = data.sprites.other[imageType].front_default;
+            evolutionImages.push(pokeImageUrl);
+        }
+    
+        return evolutionImages;
+    } catch(err) {
+        document.querySelector('.loading-wheel').style.display = "none";
+        document.getElementById('condition').textContent = "Error: Cannot Display Evolution Cycle!";
+        document.getElementById('condition').classList.add('fs-1', 'text-danger', 'mt-5')
+    }
+    
+}
+
+const displayTypes = (types) => {
+    const typesContainer = document.getElementById("types");
+    for(const type of types) {
+        const span = document.createElement('span');
+        span.textContent = `${type[0].toUpperCase()}${type.slice(1)}`;
+        span.classList.add('me-2', 'rounded-pill', 'px-3', 'pb-1')
+        span.style.backgroundColor = "rgb(255, 255, 255, 0.3)";
+        typesContainer.insertAdjacentElement("beforeend", span);
+    }
+}
+
+const displayStats = (stats) => {
+    const statsListItems = new Array(...document.getElementsByClassName('stat'));
+    statsListItems.forEach((li, index) => {
+        li.insertAdjacentText("beforeend", stats[index].toUpperCase());
+    })
+}
+
+const displayEvolutionCycle = async (pokemon) => {
+    const cycle = await getEvolutionCycle(pokemon);
+    const imageUrls = await getEvolutionImages(cycle);
+    const evolutionCycleContainer = document.getElementById("evolution-cycle");
+    cycle.forEach((pokemon, index) => {
+        if(cycle.length == 1) {
+            document.getElementById('condition').textContent = "This pokemon does not evolve.";
+        }
+        const pokemonContainer = `<div class="text-center lead mt-3">
+                                    <div class="mx-auto border border-dark border-4 rounded-circle p-3 image-container" style="width: 60%;">
+                                        <img src=${imageUrls[index]} alt=${pokemon} class="img-fluid" loading="lazy">
+                                    </div>
+                                    <p class="mt-3 fs-3">${pokemon[0].toUpperCase() + pokemon.slice(1)}</p>
+                                  </div>`
+        
+        evolutionCycleContainer.insertAdjacentHTML("beforeend", pokemonContainer);
+
+        const nextContainer = `<div style="width: 10%;">
+                                    <img src="../assets/images/chevron.png" class="img-fluid next" loading="lazy">
+                               </div>`
+
+        if(index != cycle.length - 1) evolutionCycleContainer.insertAdjacentHTML("beforeend", nextContainer)
+    })
+
+    document.querySelector('.loading-wheel').style.display = "none";
+
+}
+
+const addBackground = (pokemonObj) => {
+    const type = pokemonObj.types[0];
+    switch(type) {
+        case 'ground':
+            document.body.style.backgroundColor = '#f7de3f';
+            break;
+
+        case 'dragon':
+            document.body.style.backgroundColor = '#53a4cf';
+            break;
+
+        case 'flying':
+            document.body.style.backgroundColor = '#3dc7ef';
+            break;
+
+        default:
+            document.body.classList.add(type);
+    }
+}
+
+const pokemon = JSON.parse(localStorage.getItem("pokemon"));
+document.querySelector('title').textContent = `${pokemon.name[0].toUpperCase()}${pokemon.name.slice(1)}`;
+document.getElementById("pokemon-name").textContent = pokemon.name.toUpperCase();
+document.querySelector('img').src = pokemon.image;
+document.querySelector('img').alt = pokemon.name;
+
+addBackground(pokemon);
+displayTypes(pokemon.types);
+displayStats(pokemon.stats);
+displayEvolutionCycle(pokemon.name);
